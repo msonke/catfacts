adb06250da119f8c62a3cf67a01840cac653a956
msonke <michaelsonke@gmail.com>
2023-10-23 14:11:08 -0700

    consolidated code, improved API call function

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Catfacts
 
-**TODO: Add description**
+**TODO**
+
+> Parse JSON Data
+> Store Data in Cache
 
 ## Installation
 
@@ -18,4 +21,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/catfacts>.
-
--- lib/catfacts.ex
+++ lib/catfacts.ex
@@ -3,35 +3,18 @@ defmodule Catfacts do
   Documentation for `Catfacts`.
   """
 
-  @doc """
-  Basically just practice with api calls in elixir.
+@doc """
+  Makes a simple API call to cat-fact.herokuapp.com/facts.
 
   ## Examples
 
-      iex> Catfacts.hello()
-      :world
+      iex> Catfacts.api_call()
+
 
   """
+@response HTTPoison.get("https://cat-fact.herokuapp.com/facts")
 
-  url = "https://cat-fact.herokuapp.com/facts"
-
-  case HTTPoison.get(url) do
-    {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-      IO.puts body
-    {:ok, %HTTPoison.Response{status_code: 404}} ->
-      IO.puts "Not found :("
-    {:error, %HTTPoison.Error{reason: reason}} ->
-      IO.inspect reason
-  end
-
-  # defmodule Json_Decoder do
-  #   @spec decode_response(response :: %HTTPoison.Response{}) :: {:ok, map()} | {:error, any}
-  #   def decode_response(response) do
-  #     case Jason.decode(response.body) do
-  #       {:ok, json} -> {:ok, json}
-  #       {:error, _reason} -> {:error, "Failed to decode JSON"}
-  #     end
-  #   end
-  # end
-
+def response do
+  IO.inspect @response
+end
 end
